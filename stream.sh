06bd7b6b903519bf69f173fe8112603989b315fb
Aman Mulani <86338024+amaan7744@users.noreply.github.com>
2026-01-02 23:58:04 +0530

    Create stream.sh

---
+++ stream.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+ffmpeg -re \
+  -stream_loop -1 -i rain/video/rain_night.mp4 \
+  -stream_loop -1 -i rain/audio/rain.wav \
+  -map 0:v -map 1:a \
+  -c:v copy \
+  -c:a aac -b:a 128k \
+  -af "aresample=async=1" \
+  -f flv "$RAIN_RTMP"
