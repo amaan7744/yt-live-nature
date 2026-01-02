560e75c72fd87fcf14be3d503be0f31b5b9efc8f
Aman Mulani <86338024+amaan7744@users.noreply.github.com>
2026-01-03 01:18:57 +0530

    Update stream.sh

--- rain/stream.sh
+++ rain/stream.sh
@@ -3,7 +3,7 @@ set -e
 
 ffmpeg -re \
   -stream_loop -1 -i rain/video/rain_night.mp4 \
-  -stream_loop -1 -i rain/audio/rain.MP3 \
+  -stream_loop -1 -i rain/audio/rain.mp3 \
   -map 0:v -map 1:a \
   -c:v copy \
   -c:a aac -b:a 128k \
